cdba1ecdd78d77cb4884c673ac55c1629e8f220e
mesametamaarkhan <mesamtamaark@gmail.com>
2025-04-14 12:31:03 +0500

    image div component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
+import { ImageDivComponent } from './image-div/image-div.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet, CounterComponent, ImageDivComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/image-div/image-div.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-div',
+  templateUrl: './image-div.component.html',
+  styleUrls: ['./image-div.component.css']
+})
+export class ImageDivComponent {
+  count = 0;
+}
